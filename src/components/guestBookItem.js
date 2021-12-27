3b6172c03c4aa8861f71b79614e23bce7f0eb37e
qwqw3535 <78776430+qwqw3535@users.noreply.github.com>
2021-12-27 11:53:40 +0900

    first case

---
+++ src/components/guestBookItem.js
@@ -0,0 +1,16 @@
+import { React, useState } from "react";
+
+const GuestBookItem = ({
+
+}) => {
+    return (
+        <div>
+            <input></input>
+            <textarea></textarea>
+            <button>Write</button>
+        </div>
+    )
+}
+
+
+export default GuestBookItem;
