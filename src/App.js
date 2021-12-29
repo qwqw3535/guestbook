a7967a57959791dfbc35f60ac664e80d0615c8d4
qwqw3535 <78776430+qwqw3535@users.noreply.github.com>
2021-12-29 16:34:11 +0900

    reload done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,41 @@ import GuestBookItem from "./components/guestBookItem";
 const App = () => {
   const [data, setData] = useState(null);
   const [loaded, setloaded] = useState(false);
+  const [submitted, setsubmitted] = useState(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const onContentChange = (e) => {
+    setContent(e.target.value);
+  };
+  const clickHandler = () => {
+    submit();
+    setTitle("");
+    setContent("");
+  };
+
+  const submit = () => {
+    const post = {
+      title: title,
+      content: content,
+    };
+    console.log("submitted");
+    axios({
+      method: "POST",
+      url: "http://localhost:3306/lists",
+      data: post,
+    })
+      .then((res) => {
+        console.log("axios is working " + res.data.title);
+        setsubmitted(!submitted);
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error(error);
+      });
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +61,7 @@ const App = () => {
         });
     };
     fetchData();
-  }, []);
+  }, [submitted]);
   return !loaded ? (
     <div>loading</div>
   ) : (
@@ -45,6 +80,11 @@ const App = () => {
       }}
     >
       <NewGuestBookItem
+        title={title}
+        content={content}
+        onTitleChange={onTitleChange}
+        onContentChange={onContentChange}
+        clickHandler={clickHandler}
         style={{ position: "relative", top: "200px" }}
       ></NewGuestBookItem>
       <div>
--- src/components/newGuestBookItem.js
+++ src/components/newGuestBookItem.js
@@ -1,70 +1,36 @@
 import { React, useState } from "react";
 import axios from "axios";
-const NewGuestBookItem = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+const NewGuestBookItem = ({
+  title,
+  content,
+  onTitleChange,
+  onContentChange,
+  clickHandler,
+}) => {
+  console.log(title);
+  console.log(content);
+  console.log(onTitleChange);
+  console.log(onContentChange);
+  console.log(clickHandler);
 
-  const titleChange = (e) => {
-    setTitle(e.target.value);
-  };
-  const contentChange = (e) => {
-    setContent(e.target.value);
-  };
-  const onClick = () => {
-    submit();
-
-    setTitle("");
-    setContent("");
-  };
-
-  const submit = () => {
-    const post = {
-      title: title,
-      content: content,
-    };
-    console.log("submitted");
-    axios({
-      method: "POST",
-      url: "http://localhost:3306/lists",
-      data: post,
-    })
-      .then((res) => {
-        console.log("axios is working " + res.data.title);
-      })
-      .catch((error) => {
-        console.log(error);
-        throw new Error(error);
-      });
-    axios({
-      method: "GET",
-      url: "http://localhost:3306/lists",
-    })
-      .then((res) => {
-        console.log("axios is working " + res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        throw new Error(error);
-      });
-  };
   return (
     <div style={{ width: "60%" }}>
       <input
         value={title}
-        onChange={titleChange}
+        onChange={onTitleChange}
         placeholder="Title"
         style={{ width: "100%", margin: "5px", marginTop: "20px" }}
       />
       <div />
       <textarea
         value={content}
-        onChange={contentChange}
+        onChange={onContentChange}
         placeholder="Content"
         style={{ width: "100%", height: "100px", margin: "5px" }}
       />
       <div />
       <button
-        onClick={() => onClick()}
+        onClick={() => clickHandler()}
         style={{ float: "right", width: "15%", height: "20%" }}
       >
         Write
